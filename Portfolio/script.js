0027e2c5236d8d63f13ac661b88ad7a28d3b8e8b
Siddharth <siddhartha.marko.3@gmail.com>
2023-11-24 08:26:30 +0530

    add portfolio and add scirpt to outer html file which redirect to this portfolio

---
+++ Portfolio/script.js
@@ -0,0 +1,5 @@
+    const cursor = document.querySelector('.mouse');
+document.addEventListener('mousemove', (e) => {
+        cursor.style.left = (e.clientX - cursor.clientHeight / 2) + 'px';
+        cursor.style.top = (e.clientY - cursor.clientWidth / 2) + 'px';
+    })
