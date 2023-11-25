df7ccaea1720a26668d7a5a237db2fb5cfd80520
Siddharth <siddhartha.marko.3@gmail.com>
2023-11-25 18:49:51 +0530

    update some part

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -3,3 +3,7 @@ document.addEventListener('mousemove', (e) => {
         cursor.style.left = (e.clientX - cursor.clientHeight / 2) + 'px';
         cursor.style.top = (e.clientY - cursor.clientWidth / 2) + 'px';
     })
+document.addEventListener('mouseover', (e) => {
+    console.log('Mouse entered!', e.target);
+    
+});
